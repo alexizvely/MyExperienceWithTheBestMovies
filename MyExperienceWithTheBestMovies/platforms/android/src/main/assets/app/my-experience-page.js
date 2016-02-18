b4f94d7781bf03dec1629fc7b87b449825de5b3e
alexizvely <alexizvely@gmail.com>
2016-02-18 16:03:22 +0200

    data transitioned to my-experience page too

--- MyExperienceWithTheBestMovies/platforms/android/src/main/assets/app/my-experience-page.js
+++ MyExperienceWithTheBestMovies/platforms/android/src/main/assets/app/my-experience-page.js
@@ -1,6 +1,13 @@
-var vmModule = require("./main-view-model");
-function pageLoaded(args) {
-    var page = args.object;
-    page.bindingContext = vmModule.mainViewModel;
+var vmModule = require("./my-experience-view-model");
+var frameModule = require("ui/frame");
+
+function onNavigatedTo(args) {
+  console.log('my-experience-page-> onNavigatedTo');
+  var page = args.object;
+  var data = vmModule.viewModel;
+  data.selectedMovie = args.object.navigationContext.selectedMovie;
+  page.bindingContext = vmModule.viewModel;
+  console.log("Selected movie: " + args.object.navigationContext.selectedMovie);
 }
-exports.pageLoaded = pageLoaded;
+
+exports.onNavigatedTo = onNavigatedTo;
