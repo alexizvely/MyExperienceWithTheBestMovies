30593a25ad56ec665ff48faa9ba211dba6deafa2
kasav <ka.valkov81@gmail.com>
2016-02-19 21:31:03 +0200

    Added 3 npm packages, animation (from package) on main page load, animation (native fade and button background) on details page

--- MyExperienceWithTheBestMovies/app/details-page.js
+++ MyExperienceWithTheBestMovies/app/details-page.js
@@ -1,5 +1,6 @@
 var vmModule = require("./details-view-model");
 var frameModule = require("ui/frame");
+var colorModule = require("color");//for animation color change
 
 function onNavigatedTo(args) {
     console.log('details-page-> onNavigatedTo');
@@ -8,6 +9,15 @@ function onNavigatedTo(args) {
     data.selectedMovie = args.object.navigationContext.selectedMovie;
     page.bindingContext = vmModule.viewModel;
     console.log("Selected movie: " + args.object.navigationContext.selectedMovie);
+
+
+    //on load animation
+    var viewToAppear = page.getViewById("detailsPage");
+    var buttonToAppear = page.getViewById("experienceButton");
+    viewToAppear.animate({ opacity: 1, duration: 1000 })
+    .then(function () { return buttonToAppear.animate({ backgroundColor: new colorModule.Color("#fad417"), duration: 1000 }); })
+    .then(function () { console.log("Animation finished"); })
+    .catch(function (e) { console.log(e.message); });
 }
 
 exports.onNavigatedTo = onNavigatedTo;
@@ -18,12 +28,12 @@ exports.onNavigatedTo = onNavigatedTo;
 
 function increaseFontSize(args) {
     console.log('longPress');
-	args.object.fontSize+=1;
+	args.object.fontSize += 1;
 }
 exports.increaseFontSize = increaseFontSize;
 
 function decreaseFontSize(args) {
     console.log('doubleTap');
-	args.object.fontSize-=1;
+	args.object.fontSize -= 1;
 }
 exports.decreaseFontSize = decreaseFontSize;
