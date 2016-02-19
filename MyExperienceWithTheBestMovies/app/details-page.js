4dcf49d3246c5087660f1243a7953bbcef3352ad
kasav <ka.valkov81@gmail.com>
2016-02-19 16:04:25 +0200

    Details page UI, two gestures

--- MyExperienceWithTheBestMovies/app/details-page.js
+++ MyExperienceWithTheBestMovies/app/details-page.js
@@ -11,3 +11,19 @@ function onNavigatedTo(args) {
 }
 
 exports.onNavigatedTo = onNavigatedTo;
+
+
+
+//sample gestures - doubleTap and longPress  also declaired in XML of text field here
+
+function increaseFontSize(args) {
+    console.log('longPress');
+	args.object.fontSize+=1;
+}
+exports.increaseFontSize = increaseFontSize;
+
+function decreaseFontSize(args) {
+    console.log('doubleTap');
+	args.object.fontSize-=1;
+}
+exports.decreaseFontSize = decreaseFontSize;
