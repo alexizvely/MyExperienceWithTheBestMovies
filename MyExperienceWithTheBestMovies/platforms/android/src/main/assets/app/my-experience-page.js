c471fefeea410b89fcee5c7b252ff8d49dc78c8e
kasav <ka.valkov81@gmail.com>
2016-02-20 14:08:34 +0200

    Adding and listing comments, CAMERA FUCKING BROKEN

--- MyExperienceWithTheBestMovies/platforms/android/src/main/assets/app/my-experience-page.js
+++ MyExperienceWithTheBestMovies/platforms/android/src/main/assets/app/my-experience-page.js
@@ -1,37 +1,54 @@
 var vmModule = require("./main-view-model");
 var observable = require("data/observable").Observable;
+var observableArray = require("data/observable-array").ObservableArray;
 var cameraModule = require("camera");
 var imageModule = require("ui/image");
 var view = require("ui/core/view");
 
 var page;
-var imageBoxXml = view.getViewById(page, "image");;
+var imageBoxXml;
 
 function pageLoaded(args) {
     page = args.object;
-    page.bindingContext = vmModule.mainViewModel;
+    page.bindingContext = pageData;
+
+	imageBoxXml = view.getViewById(page, "image");
+    imageBoxXml.src ="~/images/imagePlaceholder.png";
 }
 exports.pageLoaded = pageLoaded;
+
+var pageData = new observable({
+    pictureList: new observableArray([
+    	{ img: "test1" },
+        { img: "test2" },
+        { img: "test3" } 
+    ]),
+    comments: new observableArray([])
+});
 
 
 function onTakePictureTap(args){
 	cameraModule.takePicture({width: 200, height: 200, keepAspectRatio: true})
 	.then(function(picture) {
-	    // console.log("Result is an image source instance");
-	    // var imageModule = new imageModule.Image();
-	    // imageModule.imageSource = picture;
+	    var imageModule = new imageModule.Image();
+	    imageModule.imageSource = picture;
+	    console.log("Result is an image source instance");
+
+	    pageData.pictureList.push(imageModule);
+		console.log(pageData.pictureList);
+
 	    imageBoxXml.imageSource = picture;
-	    console.log('picture taken');
-	});
-	
+	    console.log('picture shown');
+	   
+	 });
 }
 exports.onTakePictureTap = onTakePictureTap;
 
 function saveComment(args){
 	var commentWriteField = view.getViewById(page, "commentWriteField");
 	var comment = commentWriteField.text;
-	var showCommentField = view.getViewById(page, "showCommentField");
-	showCommentField.text = comment;
+	pageData.comments.push({string: comment});
+	commentWriteField.text="";
 	
 }
 exports.saveComment = saveComment;
