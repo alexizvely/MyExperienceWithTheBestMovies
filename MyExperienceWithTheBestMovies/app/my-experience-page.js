93798fe5d0c8f022f0bb971ef8ba4d9148df18cf
kasav <ka.valkov81@gmail.com>
2016-02-20 14:47:27 +0200

    added alerts on camera and comment button tap

--- MyExperienceWithTheBestMovies/app/my-experience-page.js
+++ MyExperienceWithTheBestMovies/app/my-experience-page.js
@@ -1,9 +1,11 @@
 var vmModule = require("./main-view-model");
 var observable = require("data/observable").Observable;
 var observableArray = require("data/observable-array").ObservableArray;
-var cameraModule = require("camera");
-var imageModule = require("ui/image");
-var view = require("ui/core/view");
+var cameraModule = require("camera");    // for camera
+var imageModule = require("ui/image");   //for images
+var view = require("ui/core/view");       //for getviewbyId
+var dialogs = require("ui/dialogs");      //for alerts
+var toastModule = require("nativescript-toast");  //for toast
 
 var page;
 var imageBoxXml;
@@ -28,27 +30,59 @@ var pageData = new observable({
 
 
 function onTakePictureTap(args){
-	cameraModule.takePicture({width: 200, height: 200, keepAspectRatio: true})
-	.then(function(picture) {
-	    var imageModule = new imageModule.Image();
-	    imageModule.imageSource = picture;
-	    console.log("Result is an image source instance");
+	dialogs.confirm({
+	  title: "Take picture",
+	  message: "Take picture using device's camera?",
+	  okButtonText: "OK",
+	  cancelButtonText: "Cancel",
+	}).then(function (result) {
+	  console.log("Dialog result: " + result);
+	  
+		  if (result) {
+				cameraModule.takePicture({width: 200, height: 200, keepAspectRatio: true})
+				.then(function(picture) {
+			    var imageModule = new imageModule.Image();
+			    imageModule.imageSource = picture;
+			    console.log("Result is an image source instance");
 
-	    pageData.pictureList.push(imageModule);
-		console.log(pageData.pictureList);
+			    pageData.pictureList.push(imageModule);
+				console.log(pageData.pictureList);
 
-	    imageBoxXml.imageSource = picture;
-	    console.log('picture shown');
-	   
-	 });
+			    imageBoxXml.imageSource = picture;
+			    console.log('picture shown');
+			});
+
+		}
+	});	
 }
 exports.onTakePictureTap = onTakePictureTap;
 
 function saveComment(args){
-	var commentWriteField = view.getViewById(page, "commentWriteField");
-	var comment = commentWriteField.text;
-	pageData.comments.push({string: comment});
-	commentWriteField.text="";
-	
+	dialogs.confirm({
+	  title: "Comment",
+	  message: "Add comment?",
+	  okButtonText: "OK",
+	  cancelButtonText: "Cancel",
+	}).then(function (result) {
+	  console.log("Dialog result: " + result);
+	  
+	  if (result) {
+			var commentWriteField = view.getViewById(page, "commentWriteField");
+			var comment = commentWriteField.text;
+
+			if (comment.length > 0) {
+			    pageData.comments.push({string: comment});
+			    commentWriteField.text="";
+			    
+			   //  var toast = toastModule.makeText("Comment added.");
+  				// toast.show();
+			} else {
+				dialogs.alert("Zero length comments not allowed!").then(function (result) {
+					// var toast = toastModule.makeText("Comment scrapped.");
+  			// 		toast.show();
+				});
+			}
+	    }
+	});	
 }
 exports.saveComment = saveComment;
